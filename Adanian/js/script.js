b8bd6b7846a0b3e3592c9406e9ee8bdc1185a704
chebon254 <kelvinchebon90@gmail.com>
2023-09-13 20:06:44 +0300

    Code Changes

--- Adanian/js/script.js
+++ Adanian/js/script.js
@@ -3,11 +3,6 @@ const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 const menu = document.querySelector(".links");
 const header = document.querySelector(".nav");
-// Onclick For Menu Bar
-// Onscroll Change Nav Bar Background
-window.onscroll = ()=>{
-    this.scrollY > 20 ? header.classList.add("sticky") : header.classList.remove("sticky");
-}
 
 menuBtn.onclick = ()=>{
     menu.classList.add("active");
@@ -19,30 +14,3 @@ cancelBtn.onclick = ()=>{
     menu.classList.remove("active");
     menuBtn.classList.remove("hide");
 }
-
-/* == Timeline == */
-const tabButtons = document.querySelectorAll('.selector-btn');
-const tabContents = document.querySelectorAll('.schedule-slide');
-
-function showTab(tabIndex) {
-  tabButtons.forEach((button) => {
-    button.classList.remove('active');
-  });
-
-  tabContents.forEach((content) => {
-    content.classList.remove('active');
-  });
-
-  tabButtons[tabIndex].classList.add('active');
-  tabContents[tabIndex].classList.add('active');
-}
-
-tabButtons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    showTab(index);
-  });
-});
-
-showTab(0); // Show the first tab by default
-
-/* == || Timeline == */
